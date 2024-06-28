999677f12f07cab2087d1cef166b800864341902
Reduzer <83287196+Reduzer@users.noreply.github.com>
2024-06-28 12:52:43 +0200

    Added the Server side of the program

--- Console Messenger/Server/TCPServer.cs
+++ Console Messenger/Server/TCPServer.cs
@@ -1,4 +1,5 @@
 using System.Net.Sockets;
+using System.Text;
 
 namespace Program.Server;
 
@@ -6,11 +7,60 @@ public class TCPServer
 {
     private static TcpListener server;
 
-    private static bool isRunning = false;
+    private static bool bIsRunning = false;
 
     private static int port = 5000;
+    StreamWriter writer;
+    StreamReader reader;
+
     public TCPServer()
     {
         
+    }
+
+    public void start()
+    {
+        Thread t = new Thread(handleCommunication);
+        t.Start();
+    }
+
+    private void handleCommunication()
+    {
+        server = new TcpListener(port);
+        server.Start();
+
+        while (bIsRunning)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Thread clientThread = new Thread(new ParameterizedThreadStart(handleClient));
+            clientThread.Start(client);
+        }
+
+    }
+
+    private void handleClient(object obj)
+    {
+        string name;
+
+        TcpClient client = (TcpClient)obj;
+
+        writer = new StreamWriter(client.GetStream(), Encoding.ASCII);
+        reader = new StreamReader(client.GetStream(), Encoding.ASCII);
+
+        bool isClientConnected = true;
+
+        string sData = String.Empty;
+
+        name = reader.ReadLine();
+
+        while (isClientConnected)
+        {
+            sData = reader.ReadLine();
+
+            if (sData != String.Empty)
+            {
+                Console.WriteLine(name + ": " + sData);
+            }
+        }
     }
 }
